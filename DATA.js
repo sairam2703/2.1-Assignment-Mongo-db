d6e1f4264c99452566761b541f6a21efe244fbe1
sairam2703 <137244653+sairam2703@users.noreply.github.com>
2023-06-24 13:18:09 +0300

    Create DATA.js

---
+++ DATA.js
@@ -0,0 +1,30 @@
+db.team.insertMany([
+  {
+    "name": "Player 1",
+    "rushingYards": 500,
+    "touchdownsThrown": 20,
+    "sacks": 10,
+    "fieldGoalsMade": 15,
+    "fieldGoalsMissed": 3,
+    "catchesMade": 40
+  },
+  {
+    "name": "Player 2",
+    "rushingYards": 350,
+    "touchdownsThrown": 15,
+    "sacks": 8,
+    "fieldGoalsMade": 10,
+    "fieldGoalsMissed": 2,
+    "catchesMade": 30
+  },
+  {
+    "name": "Player 3",
+    "rushingYards": 700,
+    "touchdownsThrown": 25,
+    "sacks": 12,
+    "fieldGoalsMade": 12,
+    "fieldGoalsMissed": 1,
+    "catchesMade": 50
+  },
+  // ... and so on
+])
